eab878aa68104ff441f8b61bf19b0e4e0b0266c6
Joe Colino <joepc74@gmail.com>
2026-04-15 18:27:14 +0200

    Arreglo de bug a la hora de comprobar si ya esta renombrado

--- renombraemails.py
+++ renombraemails.py
@@ -1,4 +1,4 @@
-import extract_msg, email, os
+import extract_msg, email, os, re
 from email import policy
 from email.utils import parsedate_to_datetime
 
@@ -21,16 +21,16 @@ def obtener_fecha_eml(ruta_archivo):
     with open(ruta_archivo, 'rb') as f:
         # Usamos policy.default para que el objeto sea más fácil de manejar
         msg = email.message_from_binary_file(f, policy=policy.default)
-    
+
     # Extraemos la cabecera 'Date'
     fecha_str = msg['date']
-    
+
     # Convertimos el string de la fecha a un objeto datetime de Python
     fecha_dt = parsedate_to_datetime(fecha_str)
-    
+
     # print(f"Fecha original (string): {fecha_str}")
     # print(f"Fecha como objeto datetime: {fecha_dt}")
-    
+
     return fecha_dt
 
 if __name__ == "__main__":
@@ -38,8 +38,8 @@ if __name__ == "__main__":
     import os
     archivos = [f for f in os.listdir('.') if f.endswith('.eml') or f.endswith('.msg')]
     for archivo in archivos:
-        # Si el archivo comienza por YYYYMMDD lo ignoro
-        if archivo[:8].isdigit():
+        # Si el archivo comienza por YYYYMMDD HHMMSS lo ignoro
+        if re.match(r'^\d{8} \d{6}', archivo):
             print(f"Archivo {archivo} ya renombrado, se ignora.")
             continue
         if archivo.endswith('.eml'):
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         elif archivo.endswith('.msg'):
             print(f"Procesando {archivo}...")
             fecha=obtener_fecha_msg(archivo)
-        # Renombro el archivo con el formato YYYYMMDD_nombreoriginal.ext
-        nuevo_nombre = f"{fecha.strftime('%Y%m%d')} {archivo}"
+        # Renombro el archivo con el formato YYYYMMDD HHMMSS nombreoriginal.ext
+        nuevo_nombre = f"{fecha.strftime('%Y%m%d %H%M%S')} {archivo}"
         os.rename(archivo, nuevo_nombre)
         print(f"Archivo {archivo} renombrado a {nuevo_nombre}.")
